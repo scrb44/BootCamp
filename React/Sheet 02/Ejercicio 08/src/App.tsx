f9cbbf784b36a7afe284e581f0488602400c2a8a
Sofía <soceriba@gmail.com>
2025-04-16 18:41:38 +0200

    feat: React Sheet 02 Ej 08
    Hacer que se cambie los estilos del body en funcion de un boton

--- React/Sheet 02/Ejercicio 08/src/App.tsx
+++ React/Sheet 02/Ejercicio 08/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+enum Tema {
+    Claro = "claro",
+    Oscuro = "oscuro",
 }
 
-export default App
+function App() {
+    const [temaActual, setTemaActual] = useState(Tema.Claro);
+
+    useEffect(() => {
+        const body = document.querySelector("body");
+
+        body?.classList.remove(Tema.Claro, Tema.Oscuro);
+        body?.classList.add(temaActual);
+    }, [temaActual]);
+    return (
+        <>
+            <button
+                onClick={() =>
+                    temaActual == Tema.Claro
+                        ? setTemaActual(Tema.Oscuro)
+                        : setTemaActual(Tema.Claro)
+                }
+            >
+                Cambiar a {temaActual === Tema.Claro ? "Oscuro" : "Claro"}
+            </button>
+        </>
+    );
+}
+
+export default App;
