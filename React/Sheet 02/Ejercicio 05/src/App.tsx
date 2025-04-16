ddc52b834ecfac54fa6a06690f87bd44c00a7a9a
Sofía <soceriba@gmail.com>
2025-04-16 12:24:21 +0200

    feat: React Sheet 02 Ej 05
    Crear un reloj que cambie en tiempo real

--- React/Sheet 02/Ejercicio 05/src/App.tsx
+++ React/Sheet 02/Ejercicio 05/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [date, setDate] = useState(new Date());
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    useEffect(() => {
+        const interval = setInterval(() => setDate(new Date()), 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const hour = date.getHours();
+    const minutes =
+        date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    const seconds =
+        date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    return (
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+            Hora actual: {hour}:{minutes}:{seconds}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
 }
 
-export default App
+export default App;
