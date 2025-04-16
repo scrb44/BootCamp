28badc7ef2071c09a3d4bf26c84167260a93442b
Sofía <soceriba@gmail.com>
2025-04-16 10:43:43 +0200

    feat: React Sheet 02 Ej 02
    Hacer un form que tome los valores y los muestre

--- React/Sheet 02/Ejercicio 02/src/App.tsx
+++ React/Sheet 02/Ejercicio 02/src/App.tsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nombre, setNombre] = useState("")
+  const [email, setEmail] = useState("")
+  const [contrasenia, setContrasenia] = useState("")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form action="#">
+        <label htmlFor="nombre">Nombre</label>
+        <input 
+          type="text" 
+          name="nombre" 
+          id='nombre' 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNombre(e.currentTarget.value)} 
+          value={nombre} 
+        />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email" 
+          name="email" 
+          id='email' 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)} 
+          value={email}
+        />
+        <br />
+        <label htmlFor="contrasenia">Contraseña</label>
+        <input 
+          type="password" 
+          name="contrasenia" 
+          id='contrasenia' 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setContrasenia(e.currentTarget.value)} 
+          value={contrasenia}
+        />
+
+      </form>
+
+      <p>Nombre: {nombre}</p>
+      <p>Email: {email}</p>
+      <p>Contraseña: {contrasenia}</p>
     </>
   )
 }
