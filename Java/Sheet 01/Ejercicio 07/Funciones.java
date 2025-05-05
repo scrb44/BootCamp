7e58e9ebaeedad23091d9816099f90a34fb9b715
Sofía <soceriba@gmail.com>
2025-05-05 12:21:42 +0200

    feat: Java Sheet 01 Ej 07
    Creacion y uso de funciones adicionales

---
+++ Java/Sheet 01/Ejercicio 07/Funciones.java
@@ -0,0 +1,12 @@
+public class Funciones {
+
+    public static void main(String[] args) {
+        int resultado = suma(4, 10);
+
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+}
