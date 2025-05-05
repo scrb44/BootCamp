94681f937278a8dfbd7ed52a43957c08d2f25922
Sofía <soceriba@gmail.com>
2025-05-05 12:18:00 +0200

    feat: Java Sheet 01 Ej 06
    Hacer un array de 5 elementos e imprimirlo por pantalla

---
+++ Java/Sheet 01/Ejercicio 06/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] lista = { "Hola", "Mundo", "Esto", "Es", "Texto" };
+
+        for (String palabra : lista) {
+            System.out.println(palabra);
+        }
+    }
+}
