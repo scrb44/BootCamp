2ab37449ac7d907a7415098e57e24719df652beb
Sofía <soceriba@gmail.com>
2025-05-05 12:47:50 +0200

    fix: Java Sheet 01 Ej 10 Aleatorio empezaba en 0
    Se ha sumado +1 al aleatorio para que efectivamente el numero generado
    aleatoriamente este comprendido entre 1 y 100

--- Java/Sheet 01/Ejercicio 10/Aleatorios.java
+++ Java/Sheet 01/Ejercicio 10/Aleatorios.java
@@ -4,7 +4,7 @@ public class Aleatorios {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final int numAdivinar = (int) (Math.random() * 100);
+        final int numAdivinar = (int) (Math.random() * 100 + 1);
         int prueba;
         boolean adivinado = false;
 
