b48d138fd5015561be357fe02f03cbe6caf3d3c4
Sofía <soceriba@gmail.com>
2025-05-05 11:00:41 +0200

    feat: Java Sheet 01 Ej 02
    Ejercicio de hacer prints de distintos tipos de variables

---
+++ Java/Sheet 01/Ejercicio 02/VariablesTipo.java
@@ -0,0 +1,20 @@
+public class VariablesTipo {
+    public static void main(String[] args) {
+        int numero = 3;
+        double numeroDoble = 2.3;
+        boolean esVerdad = true;
+        char letra = 'A';
+        String text = "Hola mundo";
+
+        System.out.print("Integer: ");
+        System.out.println(numero);
+        System.out.print("Double: ");
+        System.out.println(numeroDoble);
+        System.out.print("Boolean: ");
+        System.out.println(esVerdad);
+        System.out.print("Char: ");
+        System.out.println(letra);
+        System.out.print("String: ");
+        System.out.println(text);
+    }
+}
