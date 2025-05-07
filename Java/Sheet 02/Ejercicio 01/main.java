be2b9a2a712e6b1142e5ebfa10b432be1d6235d7
Sofía <soceriba@gmail.com>
2025-05-07 11:52:37 +0200

    feat: Java Sheet 02 Ej 01 Crear un objeto

---
+++ Java/Sheet 02/Ejercicio 01/main.java
@@ -0,0 +1,20 @@
+class Persona {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + this.nombre + "\nEdad: " + this.edad);
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        Persona juan = new Persona("Juan", 25);
+        juan.mostrarDetalles();
+    }
+}
