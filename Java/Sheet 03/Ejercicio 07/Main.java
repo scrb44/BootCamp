7634ca83ca519a6a2605d6e1bafa5f64d9938c3c
Sofía <soceriba@gmail.com>
2025-05-08 13:07:48 +0200

    feat: Java Sheet 03 Ej 07 Wrapper Classes

---
+++ Java/Sheet 03/Ejercicio 07/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String ejemplo = "123";
+        int a = Integer.parseInt(ejemplo);
+
+        a *= 2;
+
+        System.out.println("El resultado es: " + a);
+    }
+}
