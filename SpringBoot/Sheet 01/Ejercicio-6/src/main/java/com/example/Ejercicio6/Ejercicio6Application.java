43849a6959c6f51ee1c8a449b8f9f0e58f60a6ab
Sofía <soceriba@gmail.com>
2025-05-12 12:07:05 +0200

    feat: SpringBoot Sheet 01 Estructura de carpetas
    Creada la estructura de carpetas y los distintos proyectos para cada uno
    de los respectivos ejercicios

---
+++ SpringBoot/Sheet 01/Ejercicio-6/src/main/java/com/example/Ejercicio6/Ejercicio6Application.java
@@ -0,0 +1,13 @@
+package com.example.Ejercicio6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio6Application.class, args);
+	}
+
+}
