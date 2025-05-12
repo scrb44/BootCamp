b2c631a3840aa23ac84ceb45f1141fabc7244606
Sofía <soceriba@gmail.com>
2025-05-12 12:46:33 +0200

    feat: SpringBoot Sheet 01 Ej 02

--- SpringBoot/Sheet 01/Ejercicio-2/src/main/java/com/example/Ejercicio2/Ejercicio2Application.java
+++ SpringBoot/Sheet 01/Ejercicio-2/src/main/java/com/example/Ejercicio2/Ejercicio2Application.java
@@ -2,12 +2,23 @@ package com.example.Ejercicio2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class Ejercicio2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ejercicio2Application.class, args);
 	}
 
+	@GetMapping("/suma/{a}/{b}")
+	public int suma(@PathVariable int a,@PathVariable int b){
+		return a+b;
+	}
+
+	@RequestMapping(value = "/multiplica", params = {"a", "b"})
+	public int multiplica(@RequestParam int a, @RequestParam int b){
+		return a*b;
+	}
 }
