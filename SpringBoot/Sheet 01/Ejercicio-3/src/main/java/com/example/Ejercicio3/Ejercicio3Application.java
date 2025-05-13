992388563d00292cc60ba94a0abd418240bfdec8
Sofía <soceriba@gmail.com>
2025-05-13 12:23:26 +0200

    fix: SpringBoot Sheet 01 Ej 03 Automatizacion ID
    Se ha cambiado el id para que se añada de forma automática mediante un
    contador estático de la clase

--- SpringBoot/Sheet 01/Ejercicio-3/src/main/java/com/example/Ejercicio3/Ejercicio3Application.java
+++ SpringBoot/Sheet 01/Ejercicio-3/src/main/java/com/example/Ejercicio3/Ejercicio3Application.java
@@ -9,32 +9,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Frase {
-	public int id;
-	public String texto;
-	public String autor;
+	private static int contador = 1;
+	private int id;
+	private String texto;
+	private String autor;
 
-	public Frase(){}
-	public Frase(int id, String texto, String autor){
-		this.id = id;
+	public Frase(){
+		this("", "");
+	}
+	public Frase(String texto, String autor){
+		this.id = contador;
 		this.texto = texto;
 		this.autor = autor;
+
+		contador++;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
 }
 
 @SpringBootApplication
 @RestController
 public class Ejercicio3Application {
 	public static List<Frase> frases = new ArrayList<>(List.of(
-			new Frase(1, "La vida es lo que pasa mientras estás ocupado haciendo otros planes.", "John Lennon"),
-			new Frase(2, "El único modo de hacer un gran trabajo es amar lo que haces.", "Steve Jobs"),
-			new Frase(3, "Sé tú el cambio que quieres ver en el mundo.", "Mahatma Gandhi"),
-			new Frase(4, "No cuentes los días, haz que los días cuenten.", "Muhammad Ali"),
-			new Frase(5, "El éxito es aprender a ir de fracaso en fracaso sin desesperarse.", "Winston Churchill"),
-			new Frase(6, "La imaginación es más importante que el conocimiento.", "Albert Einstein"),
-			new Frase(7, "El futuro pertenece a quienes creen en la belleza de sus sueños.", "Eleanor Roosevelt"),
-			new Frase(8, "Cree que puedes y ya estás a medio camino.", "Theodore Roosevelt"),
-			new Frase(9, "No hay caminos para la paz, la paz es el camino.", "Martin Luther King Jr."),
-			new Frase(10, "Haz lo que puedas, con lo que tengas, donde estés.", "Theodore Roosevelt")
+			new Frase("La vida es lo que pasa mientras estás ocupado haciendo otros planes.", "John Lennon"),
+			new Frase("El único modo de hacer un gran trabajo es amar lo que haces.", "Steve Jobs"),
+			new Frase("Sé tú el cambio que quieres ver en el mundo.", "Mahatma Gandhi"),
+			new Frase("No cuentes los días, haz que los días cuenten.", "Muhammad Ali"),
+			new Frase("El éxito es aprender a ir de fracaso en fracaso sin desesperarse.", "Winston Churchill"),
+			new Frase("La imaginación es más importante que el conocimiento.", "Albert Einstein"),
+			new Frase("El futuro pertenece a quienes creen en la belleza de sus sueños.", "Eleanor Roosevelt"),
+			new Frase("Cree que puedes y ya estás a medio camino.", "Theodore Roosevelt"),
+			new Frase("No hay caminos para la paz, la paz es el camino.", "Martin Luther King Jr."),
+			new Frase("Haz lo que puedas, con lo que tengas, donde estés.", "Theodore Roosevelt")
 	));
 
 	public static void main(String[] args) {
@@ -52,7 +70,7 @@ public class Ejercicio3Application {
 		int i = 0;
 
 		while(i<frases.size() && f == null){
-			if(frases.get(i).id == id){
+			if(frases.get(i).getId() == id){
 				f = frases.get(i);
 			}
 			i++;
